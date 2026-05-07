f3a710b8a7c28e6d77a9ee1cf28877094bb40358
Hitesh Jadav <jadav2592@gmail.com>
2026-05-07 14:20:20 +0530

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,67 @@
+#Basic concept 
+# Arithmatic -> + , - , % , // , / ,*,**
+# comparison -> > , < , <= , >= , != , == 
+# assignment -> = , += , -= ,*= , /=,%=
+# # logical -> and ,or, not 
+# is , is not   operator 
+
+# exaple :
+#     a= [1,2]
+#     b=print(a is b ) =>true
+
+
+
+#in ,not in operator 
+
+#  this operator check object in value are exist or not 
+
+# example 
+#     print("a" in "apple") ==> true 
+
+
+
+
+#            bitwise operator 
+# this operator perform binary operation 
+
+# (& | ^ ~ << >> ) these are binary operator 
+
+
+
+#LEVEL1
+# #1-> what is the output of  10+5*2
+# # -> output is 20 because it follow division -> multiplication -> addtion -> subtracton 
+
+# print(10+5*2)
+
+
+#2-> what is the output of 10//3 and 10%3
+# 10//3=3 10%3=1
+
+#3-> guess the result of 2**3
+# =>answer is 8 (2*2*2)
+
+# #4->5>3 and 2<1
+# # o/p is 5>3 => true and 2<1 =>False
+# print(5>3,2<1)
+
+# #5-> not(5>3) guess the output 
+# # =>output is false 
+# print(not 5>3)
+
+
+#LEVEL 2
+#q1-> take two from user and print the sum , product , and remainder of these two value 
+
+# num1=int(input("Enter frist number :"))
+# numb2= int(input("Enter second numebr :"))
+
+# print("Total sum is :",num1+num2)
+# print("total production is :",num1*num2)
+
+#Q2 -> check the is even  or odd 
+num1= int (input("Enter a value :"))
+if(num1%2==0):
+    print("number is even ")
+else:
+    print("nmber is odd")    
