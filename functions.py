e5147d14a191af7cd73b2b6251f99e7da2c45f8d
Hitesh Jadav <jadav2592@gmail.com>
2026-05-09 18:33:43 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,39 @@
+# def Greet(): #defining a function
+#     return  "hello greetings from sheryians"
+
+
+# print(Greet()) #this is calling the function 
+
+# def addition(a,b):
+#     print(a + b)
+
+# addition(12,45)
+# addition(45,67)
+# addition(12,12)
+# addition(13,13)
+
+# def pallindrome(x):
+#     rev = 0 
+#     copy = x 
+#     while x >0:
+#         rev = (rev * 10) + (x % 10)
+#         x =x //10
+#     if rev == copy:
+#         return True
+#     else:
+#         return False
+
+# print(pallindrome(121))
+# print(pallindrome(454))
+# print(pallindrome(234))
+
+
+# def addition(a,b,c):
+#     print(a+b)
+
+# addition(12,c = 78,b = 67) #keyword arguments
+
+# def addition(a,c,b=67):
+#     print(a +b + c)
+
+# addition(10,10,20)
