6ebe01f8c7c670dfce6a7684bedfd49fed087df4
hitesh jadav <jadav2592@gmail.com>
2026-05-10 14:38:24 +0530

    resolved merge conflict

--- list.py
+++ list.py
@@ -1,1 +1,23 @@
-print("this is list practice file ")
+# create  a list of number , then calculate and prin the sum and average 
+
+# lists =[1,2,3,4,5,6,7,8,9,0]
+# sum =0
+# for i in lists:
+#     sum+=i
+
+# l = len(lists)
+
+# print ("sum is :",sum)
+# print("Average is :",sum /l)
+
+#Q2 find the largest  element in the list along with its position (index)
+
+l = [ 10,20,30,40,40]
+max =0
+ind=0
+for i in range(len(l)):
+    if max < l[i]:
+        ind = i
+        max= l[i]
+
+print(f"Their index position is :{ind} nad maximum value is :{max} ")        
