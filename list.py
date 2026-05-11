10e50b3d26f8eadb3da5cd60c1802538ca3ad43f
hitesh jadav <jadav2592@gmail.com>
2026-05-11 14:57:59 +0530

    first commit

--- list.py
+++ list.py
@@ -81,11 +81,72 @@
 #Q7 reverse the entire list without using extra space (i.e.,swap element )
 
 
-l=[1,2,3,4,5,6,7,8,9,0]
-b = len(l)-1
-for i in range(len(l)//2):
-        l[i],l[b]=l[b],l[i]
-        b-=1
-print(l)
+# l=[1,2,3,4,5,6,7,8,9,0]
+# b = len(l)-1
+# for i in range(len(l)//2):
+#         l[i],l[b]=l[b],l[i]
+#         b-=1
+# print(l)
 
 
+# Q8 Linear search  , search for a given element by checking each element one by one
+
+# l=[1,2,3,4,5,6,7,8,9,0]
+# n=int(input("Enter number for search :"))
+
+# for i in range(len(l)):
+#         if n == l[i]:
+#                 print("Your number is found ")
+#                 break
+# else:
+#         print("your number is not found ..")        
+
+
+#Q9 Binary search , Efficiently search for an element in a sorted list using the fivide and conquer approach
+# l=[1,2,3,4,5,6,7,8,9,0]
+# n = int(input("Enter yor search number :"))
+# start = 0 
+# last = len(l)-1
+# mid = (start + last )//2
+
+# while start < last:
+#     if n == l[mid]:
+#         print("your number is found ...")
+#         break
+#     elif n > l[mid]:
+#         start=mid+1
+#         mid = (start + last )//2
+#     elif n < l[mid]:
+#         last=mid-1
+#         mid = (start + last )//2
+# else:
+#     print("Sorry your number is not found")        
+
+
+
+
+#Q10 bubble sort , sort the list by repeatedly swapping adjacent element if the are in the wrong order 
+
+# l = [11,2,32,4,6,4,5,6,4,3,2,34]
+
+# for i in range(len(l)-1):
+#     for j in range(len(l)-1-i):
+#         if l[j]>l[j+1]:
+#             temp = l[j]
+#             l[j]=l[j+1]
+#             l[j+1]=temp
+# print(l)            
+
+
+#Q11 selection sort , sort th list by selecting the smallest element in each pass and placing it in the correct position 
+l = [12,4,3,2,34,4,45,46,5,56,235,2,342,12,323,425]
+
+for i in range (len(l)-1):
+    j=i+1
+    min = i
+    for k in range(j,len(i)):
+        if l[k] < l[min] :
+            min = k
+
+    l[i],l[min]=l[min],l[i] 
+print(l)       
